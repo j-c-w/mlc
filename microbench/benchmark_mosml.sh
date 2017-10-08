8619423fb45a8aa65bc3fd3f13d420fbd8e3cb72
Jackson Woodruff <woodruff.jackson@gmail.com>
2017-10-08 12:08:01 +0100

    Add a benchmarking script for MOSML

---
+++ microbench/benchmark_mosml.sh
@@ -0,0 +1,21 @@
+
+#!/bin/bash
+
+usage() {
+	cat <<EOF
+	Usage:
+
+	$0 <Machine Name> <rev number>
+EOF
+}
+
+if [ "$#" -ne 2 ]; then
+	usage
+	exit  1
+fi
+
+# For this, we treat the version as the UNIX timestamp.
+version=$2
+
+# This program benchmarks MOSML.
+./microrun.py --mosml --machine-name "$1-mosml" --project-version $version --runs 20 --json-dump ./reports/current/mosml_no_perf.json
