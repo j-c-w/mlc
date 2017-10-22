962138d4b0a6c816808c916d30e1eb435a935aeb
Jackson Woodruff <woodruff.jackson@gmail.com>
2017-10-22 10:26:12 +0100

    Remove ASTTypedProgram and add optional variable to replace it

--- src/main/scala/frontend/ASTProgram.scala
+++ src/main/scala/frontend/ASTProgram.scala
@@ -5,29 +5,12 @@ import toplev.GenericPrintable
 object ASTProgram
 
 class ASTProgram(val decs: List[ASTDeclaration]) extends GenericPrintable {
-  def prettyPrint = 
+  var env: Option[ASTTypeEnv] = None
+
+  def prettyPrint = {
     (decs map (_.prettyPrint)).mkString("\n")
+  }
 
   override def toString =
     (decs map (_.toString)).mkString("\n")
-}
-
-class TypedASTProgram(decs: List[ASTDeclaration],
-                      val env: ASTTypeEnv) extends ASTProgram(decs) {
-  override def prettyPrint = """
-  %s
-
-  =====
-
-  %s
-  """.format(env.prettyPrint, super.prettyPrint)
-
-  override def toString = """
-  %s
-
-  =======
-
-  %s
-  """.format(env.prettyPrint, super.toString)
-
 }
