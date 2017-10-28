2397ac8eb73dc79944f5e9707d3d1d598c82f71c
Jackson Woodruff <woodruff.jackson@gmail.com>
2017-10-28 20:00:37 +0100

    Add a getType function

--- src/main/scala/frontend/ASTConst.scala
+++ src/main/scala/frontend/ASTConst.scala
@@ -6,12 +6,16 @@ import toplev.GenericPrintable
 
 object ASTConst
 
-sealed trait ASTConst extends GenericPrintable
+sealed trait ASTConst extends GenericPrintable {
+  def getType(): ASTType
+}
 
 // We use the really large formats so that the compiler
 // doesn't crash on input.
 case class ASTConstInt(val int: BigInteger) extends ASTConst {
   def prettyPrint = int.toString()
+
+  def getType = ASTIntType()
 }
 
 // The ASTConstFloat class requires a companion object to 
@@ -25,17 +29,25 @@ object ASTConstFloat {
 
 case class ASTConstFloat(val float: BigDecimal) extends ASTConst {
   def prettyPrint = float.toString()
+
+  def getType = ASTRealType()
 }
 
 case class ASTConstString(val string: String) extends ASTConst {
   def prettyPrint = "\"" + string + "\""
+
+  def getType = ASTStringType()
 }
 
 case class ASTConstChar(val char: Char) extends ASTConst {
   def prettyPrint = "#" + char
+
+  def getType = ASTCharType()
 }
 
-sealed trait ASTConstBool extends ASTConst
+sealed trait ASTConstBool extends ASTConst {
+  def getType = ASTBoolType()
+}
 
 case class ASTConstTrue() extends ASTConstBool {
   def prettyPrint = "true"
