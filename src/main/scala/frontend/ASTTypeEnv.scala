8275a727692f31e527afffbfad4e62eb5a1cbf88
Jackson Woodruff <woodruff.jackson@gmail.com>
2017-10-29 10:14:27 +0000

    All functions now typed

--- src/main/scala/frontend/ASTTypeEnv.scala
+++ src/main/scala/frontend/ASTTypeEnv.scala
@@ -1,5 +1,7 @@
 package frontend
 
+import exceptions.ICE
+import stdlib.StandardLibraries
 import toplev.GenericTypeEnv
 import typecheck.TypeVariableGenerator
 
@@ -8,9 +10,13 @@ class ASTTypeEnv(parent: Option[ASTTypeEnv])
   def this() = this(None)
 
   override def get(ident: ASTIdent) = ident match {
-    case ASTLongIdent(names) => ??? // To do, implement the standard
-    // libraries for this case. Unlikely to want to put those here,
-    // they can go in their own package
+    case ASTLongIdent(names) => {
+      StandardLibraries(names map {
+        case ASTIdentVar(name) => name
+        case ident => throw new ICE("""  Long Ident composed of non string 
+          components: %s """ + ident.prettyPrint)
+      })
+    }
     case ASTIdentTuple(_) => None // This is not handled here.
     case ASTUnderscoreIdent() => Some(TypeVariableGenerator.getVar())
     case ASTConsIdent() => {
@@ -24,6 +30,7 @@ class ASTTypeEnv(parent: Option[ASTTypeEnv])
     }
     case ASTEmptyListIdent() =>
       Some(ASTListType(TypeVariableGenerator.getVar()))
+    case ASTUnitIdent() => Some(ASTUnitType())
     case ASTMinusIdent() | ASTPlusIdent() |
          ASTTimesIdent() | ASTDivIdent() => {
       val numType = TypeVariableGenerator.getNumberTypeVar()
@@ -32,7 +39,52 @@ class ASTTypeEnv(parent: Option[ASTTypeEnv])
           List(numType, numType)),
         numType))
     }
-    // case ASTUnitIdent() => ASTUnitType()
-    case x => super.get(x)
+    case ASTAppendIdent() => {
+      val listType = TypeVariableGenerator.getVar()
+
+      Some(ASTTypeFunction(
+        ASTTypeTuple(List(
+          ASTListType(listType),
+          ASTListType(listType))),
+        ASTListType(listType)))
+    }
+    case ASTLEQIdent() | ASTLTIdent() |
+         ASTGTIdent()  | ASTGEQIdent() => {
+      val numType = TypeVariableGenerator.getNumberTypeVar()
+
+      Some(ASTTypeFunction(
+        ASTTypeTuple(List(
+          numType,
+          numType)),
+        ASTBoolType()))
+    }
+    case ASTEqIdent() => {
+      val eqType = TypeVariableGenerator.getEqualityVar()
+
+      Some(ASTTypeFunction(
+        ASTTypeTuple(List(
+          eqType,
+          eqType)),
+        ASTBoolType()))
+    }
+    case ASTUnOpNegate() => {
+      val numType = TypeVariableGenerator.getNumberTypeVar()
+      Some(ASTTypeFunction(numType, numType))
+    }
+    case ASTUnOpNot() => {
+      Some(ASTTypeFunction(
+        ASTBoolType(), ASTBoolType()))
+    }
+    case x => super.get(x) match {
+      case Some(y) => Some(y)
+      case None =>
+        // There are some things, like 'print', that we have to match
+        // last.
+        x match {
+          case ASTIdentVar("print") =>
+            Some(ASTTypeFunction(ASTStringType(), ASTUnitType()))
+          case _ => None
+        }
+    }
   }
 }
