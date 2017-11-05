f7e71b202aeccd7e975a8738f3e903460473c51f
Jackson Woodruff <woodruff.jackson@gmail.com>
2017-11-05 10:28:47 +0000

    Represent functions as lists of match rows

--- src/main/scala/tir/TDec.scala
+++ src/main/scala/tir/TDec.scala
@@ -2,33 +2,30 @@ package tir
 
 import toplev.GenericPrintable
 
-trait TDec extends TWalkable with GenericPrintable
+sealed trait TDec extends TWalkable with GenericPrintable
 
-case class TFun(var typ: TType, var patterns: List[TPat],
-                var expressions: List[TExp]) extends TDec {
+case class TFun(var name: TIdent, var patterns: List[TExpMatchRow])
+    extends TDec {
   def prettyPrint = """
-  fun: %s
-    %s """.format(typ.prettyPrint, (patterns zip expressions).map {
-      case (pat, exp) => pat.prettyPrint + " => " + exp.prettyPrint
-    }.mkString("\n    | "))
+  fun f %s
+    %s """.format(name.prettyPrint,
+                  patterns.map(_.prettyPrint).mkString("\n    | "))
 
   def walk(f: TPass) = 
     if (f(this)) {
-      typ.walk(f)
+      name.walk(f)
       patterns.foreach(_.walk(f))
       expressions.foreach(_.walk(f))
     }
 }
 
-case class TVal(var typ: TType, var ident: TIdent,
-                var exp: TExp) extends TDec {
+case class TVal(var ident: TIdent, var exp: TExp) extends TDec {
   def prettyPrint = """
   val :s (%s) = %s
-  """.format(typ.prettyPrint, ident.prettyPrint, exp.prettyPrint)
+  """.format(ident.prettyPrint, exp.prettyPrint)
 
   def walk(f: TPass) = 
     if (f(this)) {
-      typ.walk(f)
       ident.walk(f)
       exp.walk(f)
     }
