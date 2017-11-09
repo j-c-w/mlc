19c2f2fd6ac4b0716b7cd944345ab08adff382fa
Jackson Woodruff <woodruff.jackson@gmail.com>
2017-11-09 09:42:40 +0000

    Block print from being used as an identifier

--- src/main/scala/frontend/GLLParser.scala
+++ src/main/scala/frontend/GLLParser.scala
@@ -138,8 +138,8 @@ object GLLParser extends Pass[String, ASTProgram]("ast")
       "if[^A-Za-z0-9_']|then[^A-Za-z0-9_']|else[^A-Za-z0-9_']|" +
       "fn[^A-Za-z0-9_']|nil[^A-Za-z0-9_']|let[^A-Za-z0-9_']|" +
       "in[^A-Za-z0-9_']|end[^A-Za-z0-9_']|orelse[^A-Aa-z0-9_']|" +
-      "andalso[^A-Za-z0-9_']|mod[^A-Za-z0-9_']|div[^A-Za-z0-9_'])" +
-      "([A-Za-z][A-Za-z0-9_']*)").r
+      "andalso[^A-Za-z0-9_']|mod[^A-Za-z0-9_']|div[^A-Za-z0-9_']|" +
+      "print[^A-Za-z0-9_'])([A-Za-z][A-Za-z0-9_']*)").r
                          ^^  { (str) => ASTIdentVar(str) }
   )
 
