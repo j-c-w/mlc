b3ef762d7abfb4d63f8cb84fd40509087eb6bbd5
Jackson Woodruff <woodruff.jackson@gmail.com>
2017-11-14 09:26:22 +0000

    Add a verification pass for the lambda lifter

--- src/main/scala/lambda_lift/LambdaLiftVerify.scala
+++ src/main/scala/lambda_lift/LambdaLiftVerify.scala
@@ -1,8 +1,41 @@
-// package lambda_lift
+package lambda_lift
 
+import exceptions.ICE
+import tir._
+import tpass.TPass
+import toplev.OptionalPass
 
-// TODO -- This class should contain a walk to the tree to make sure that:
-// (1) there are no Fns left
-// (2) there are no Fun decs left within let-in expressions.
+/* This class contains a walk to the tree to make sure that:
+ *   (1) there are no Fns left
+ *   (2) there are no Fun decs left within let-in expressions.
+ *
+ * The actual integrity of the tree will be checked at a later date.  */
 
-// The actual integrity of the tree will be checked at a later date.
+object LambdaLiftVerify extends OptionalPass[TProgram]("verify")
+                        with TPass[Unit, Unit] {
+  override def combine(x: Unit, y: Unit) = x
+  override def default: Unit = ()
+
+  override def apply(u: Unit, exp: TExp) = exp match {
+    case tlet @ TExpLetIn(decs, innerExp, env) => {
+      decs.foreach {
+        case TFun(_, _) => throw new ICE("""Error, found a remaining
+          |inner fundec in:
+          |%s""".stripMargin.format(tlet.prettyPrint))
+        case _ =>
+      }
+
+      // Then walk the rest of the let normally:
+      super.apply(u, tlet)
+    }
+    case fndec @ TExpFn(_, _) => throw new ICE("""Error, found a
+      |emaining fndec as
+      |%s""".stripMargin.format(fndec.prettyPrint))
+    case _ => super.apply(u, exp)
+  }
+
+  def run(tree: TProgram) = {
+    apply((), tree)
+    tree
+  }
+}
