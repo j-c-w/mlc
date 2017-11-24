7eda80532bf77857e32139fce3f06b016c558eeb
Jackson Woodruff <woodruff.jackson@gmail.com>
2017-11-24 16:54:22 +0000

    Fix bug with loop introduced in another case

--- src/main/scala/typecheck/HindleyMilner.scala
+++ src/main/scala/typecheck/HindleyMilner.scala
@@ -474,7 +474,7 @@ object HindleyMilner extends Pass[ASTProgram, ASTProgram]("typecheck") {
       // Finally, compute the unification of the result types
       // to ensure that those are all the same.
       // And that any assigned types in the pattern propagate through.
-      val resultTypeUnifier = resultType unify (expUnifier(expType))
+      val resultTypeUnifier = (expUnifier(expType)) unify resultType
       resultTypeUnifier mguUnifyAll patUnifier
 
       resultType = resultTypeUnifier(resultType)
