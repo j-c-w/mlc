fbc771f46e55106d98a384058ea9a34b1f2bf527
Jackson Woodruff <woodruff.jackson@gmail.com>
2017-11-29 22:28:43 +0000

    Fixed the environment soundness walk to deal with long identifiers

--- src/main/scala/environment_soundness/EnvironmentSoundnessWalk.scala
+++ src/main/scala/environment_soundness/EnvironmentSoundnessWalk.scala
@@ -7,11 +7,17 @@ import tpass.TTypeEnvUnitPass
 object EnvironmentSoundnessWalk extends TTypeEnvUnitPass {
   override def apply(env: TTypeEnv, ident: TIdent) = ident match {
     // We should check all types of variable.
-    case variable: TNamedIdent =>
-      if (!env.hasType(variable)) {
-        throw new ICE("""Error: Variable %s is not typed by the envrionment""".
-          format(variable.prettyPrint))
-      }
+    case variable: TNamedIdent => variable match {
+      case  TTopLevelIdent(_)
+          | TIdentVar(_)
+          | TNumberedIdentVar(_, _)
+          | TArgumentNode(_, _) =>
+        if (!env.hasType(variable)) {
+          throw new ICE("""Error: Variable %s is not typed by the envrionment""".
+            format(variable.prettyPrint))
+        }
+      case TIdentLongVar(_) => // Do nothing.
+    }
     case other => super.apply(env, other)
   }
 }
