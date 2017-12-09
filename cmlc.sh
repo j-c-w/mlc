9035f99aed0a383fd3379a88b9c37d3c13e55b8e
Jackson Woodruff <woodruff.jackson@gmail.com>
2017-12-09 09:36:37 +0000

    CMLC executable now stores class files in a temp directory

--- cmlc.sh
+++ cmlc.sh
@@ -7,23 +7,26 @@
 # be set by the installer!
 LIB=AUTOMATICALLY_REPLACED_LIBRARY_LOCATION
 JAR=AUTOMATICALLY_REPLACED_JAR_LOCATION
+ASSEMBLER=AUTOMATICALLY_REPLACED_ASSEMBLER_LOCATION
+TEMP_DIR=$(mktemp -d)
 
 set -e
 set -u
 
-# Name of the file without the extension.
 base=$(basename ${@:$#})
+# Name of the file without the extension.
 filename="${base%.*}"
 
 # Build program into a .j file:
 java -jar "$JAR"/cmlc.jar "$@"
 # Then pass on to the assembler: (Note that the last argument is the file
-# name)
-krakatau "$filename.j"
+# name).  Output of Krakatau thrown away becase we don't want the screen filled
+# with millions of classes.
+$ASSEMBLER "$filename.j" -out "$TEMP_DIR" > /dev/null || (echo "Assemble failed.  Please report this as a bug. "; exit 1)
 # First create the jar file with the local classes
-jar cfe "$filename.jar" Main *.class
+jar cfe "$filename.jar" Main -c "$TEMP_DIR" .
 # Then we package this with the standard libraries:
 jar uf "$filename.jar" -C "$LIB" cmlc
 
 # Finally clean up
-rm *.class
+rm $TEMP_DIR/*.class
