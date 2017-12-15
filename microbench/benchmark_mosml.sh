22bf2ebaeec244a79e318970b2d16b7968144c23
Jackson Woodruff <woodruff.jackson@gmail.com>
2017-12-15 22:40:33 +0000

    Update mosml benchmark to use perf

--- microbench/benchmark_mosml.sh
+++ microbench/benchmark_mosml.sh
@@ -18,4 +18,4 @@ fi
 version=$2
 
 # This program benchmarks MOSML.
-./microrun.py --mosml --machine-name "$1-mosml" --project-version $version --runs 20 --json-dump ./reports/current/mosml_no_perf.json
+./microrun.py --mosml --machine-name "$1-mosml" --use-perf ./reports/current --project-version $version --runs 20 --json-dump ./reports/current/mosml_no_perf.json
