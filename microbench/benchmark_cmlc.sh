793aa98177254a6d62d4e19b325ccb3e81a1160c
Jackson Woodruff <woodruff.jackson@gmail.com>
2017-12-15 22:39:06 +0000

    Benchmark cmlc now works with new microbench

--- microbench/benchmark_cmlc.sh
+++ microbench/benchmark_cmlc.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 usage() {
 	cat <<EOF
 	Usage:
@@ -17,12 +19,17 @@ version=$2
 compiler=$3
 
 run_microrun() {
-	local machine_name = $1
-	local json_dump = $2
-	local additional_opts = $3
+	local machine_name="$1"
+	local json_dump="$2"
+	local additional_opts="$3"
 
-	./microrun.py --cmlc --machine-name $machine_name --project-version=$version --runs 20 --json-dump ./reports/current/$json_dump --compiler "$compiler" $additional_opts
+	./microrun.py --cmlc --machine-name "$machine_name" --project-version=$version --runs 5 --use-perf ./reports/current --json-dump ./reports/current/$json_dump --compiler "$compiler" $additional_opts
 }
+
+source lnt-install/bin/activate
+
+# Clear the reports directory:
+rm ./reports/current/*.{perf,json}
 
 # This program benchmarks CMLC.
 run_microrun "$1-cmlc" "cmlc-no-jit.json" ""
@@ -31,7 +38,8 @@ run_microrun "$1-cmlc" "cmlc-no-jit.json" ""
 run_microrun "$1-cmlc-jit" "cmlc-with-jit.json" "--use-jit"
 
 # And now do both, again, but with optimizations enabled
-run_microrun "$1-cmlc-optimized-no-jit" "cmlc-optimize.json" "--compile-options '-O'"
+run_microrun "$1-cmlc-optimized-no-jit" "cmlc-optimize.json" "--compile-options '-o'"
 
-run_microrun "$1-cmlc-optimized-with-jit" "cmlc-optimize-with-jit.json" "--compile-options '-O' --use-jit"
+run_microrun "$1-cmlc-optimized-with-jit" "cmlc-optimize-with-jit.json" "--compile-options '-o' --use-jit"
 
+deactivate
