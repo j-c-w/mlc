1c4c2a8ce47d149ae6156db96a8b72e0cc56f561
Jackson Woodruff <woodruff.jackson@gmail.com>
2017-12-15 13:34:25 +0000

    Clean up installation scripts to make them easier to use

--- microbench/lnt_import.sh
+++ microbench/lnt_import.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 source lnt-install/bin/activate
 
 # Import the reports
--- microbench/lnt_install.sh
+++ microbench/lnt_install.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
 
 set -e
+# We cannot set -u as lnt activate fails with it.
+# set -u
+
+if [ -d lnt-install ]; then
+	read -p "lnt-install exists.  Overwrite?[y/n]" resp
+
+	if [ "$resp" = "y" ]; then
+		rm -rf lnt-install
+		echo "Continuing"
+	else
+		echo "Install aborted"
+		exit 1
+	fi
+fi
+
+if [ -d lnt ]; then
+	read -p "lnt exists. Overwrite?[y/n]" resp
+
+	if [ "$resp" = "y" ]; then
+		rm -rf lnt
+		echo "Continuing"
+	else
+		echo "Install aborted"
+		exit 1
+	fi
+fi
 
 sudo easy_install virtualenv
 
@@ -11,6 +37,11 @@ source lnt-install/bin/activate
 pip install argparse==1.3.0
 
 svn co http://llvm.org/svn/llvm-project/lnt/trunk lnt
+
+if [ ! -d lnt ]; then
+	echo "Clone of LNT failed!"
+	exit 1
+fi
 
 lnt-install/bin/python lnt/setup.py develop
 
