14b4a20d77f1d6fa342c02fdb77a85ff23374401
Jackson Woodruff <woodruff.jackson@gmail.com>
2018-01-03 09:38:47 +0100

    Fix formatting of ICEs

--- src/main/scala/exceptions/ICE.scala
+++ src/main/scala/exceptions/ICE.scala
@@ -3,4 +3,4 @@ package exceptions
 class ICE(val msg: String) extends RuntimeException(
   """Internal compiler error: %s
   | Please submit a bug report, including source if possible
-  | or this trace if not. """.format(msg))
+  | or this trace if not. """.stripMargin.format(msg))
