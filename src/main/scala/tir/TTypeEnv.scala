d4c89ac2261b7f6aa0ab49874943b57fd026ac6d
Jackson Woodruff <woodruff.jackson@gmail.com>
2018-01-05 12:38:59 +0000

    Add utility method for extracting function argyment types

--- src/main/scala/tir/TTypeEnv.scala
+++ src/main/scala/tir/TTypeEnv.scala
@@ -1,5 +1,6 @@
 package tir
 
+import exceptions.ICE
 import toplev.GenericTypeEnv
 import tpass.TPass
 import typecheck.TypeVariableGenerator
@@ -25,4 +26,20 @@ class TTypeEnv(parent: Option[TTypeEnv])
     case TUnderscoreIdent() => TypeVariableGenerator.getTVar()
     case other => super.getOrFail(other)
   }
+
+  /* Given some function identifier with at least N curries, return
+   * the first N curried arguments as a list.
+   */
+  def getNCurriedTypesFrom(n: Int, ident: TNamedIdent) =
+    extractNCurriedTypesFrom(n, super.getOrFail(ident))
+
+  private def extractNCurriedTypesFrom(n: Int, typ: TType): List[TType] =
+    (n, typ) match {
+      case (0, _) => List[TType]()
+      case (n, TFunctionType(from, to)) =>
+        from :: extractNCurriedTypesFrom(n - 1, to)
+      case (n, other) =>
+        throw new ICE("""Function assumed to have more curried arguments than it
+                      | actually has""".stripMargin)
+    }
 }
