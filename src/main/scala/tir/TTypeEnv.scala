5a6c7b94c86df9de5519601354ce68877ce687ad
Jackson Woodruff <woodruff.jackson@gmail.com>
2018-01-08 02:38:17 +0000

    Add more utility methods for manipulating function types

--- src/main/scala/tir/TTypeEnv.scala
+++ src/main/scala/tir/TTypeEnv.scala
@@ -28,16 +28,31 @@ class TTypeEnv(parent: Option[TTypeEnv])
   }
 
   /* Given some function identifier with at least N curries, return
+   * the Nth argument.
+   */
+  def getNthResultFrom(n: Int, ident: TNamedIdent) =
+    extractNCurriedTypesFrom(n, super.getOrFail(ident))._2
+
+  /* Given some function identifier with at least N curries, return
    * the first N curried arguments as a list.
    */
   def getNCurriedTypesFrom(n: Int, ident: TNamedIdent) =
+    extractNCurriedTypesFrom(n, super.getOrFail(ident))._1
+
+  /* Given some function with at least N curries, return the first
+   * N curred arguments as a list and the last curry as an element.
+   */
+  def splitFunctionAt(n: Int, ident: TNamedIdent) =
     extractNCurriedTypesFrom(n, super.getOrFail(ident))
 
-  private def extractNCurriedTypesFrom(n: Int, typ: TType): List[TType] =
+  private def extractNCurriedTypesFrom(n: Int,
+                                       typ: TType): (List[TType], TType) =
     (n, typ) match {
-      case (0, _) => List[TType]()
-      case (n, TFunctionType(from, to)) =>
-        from :: extractNCurriedTypesFrom(n - 1, to)
+      case (0, typ) => (List[TType](), typ)
+      case (n, TFunctionType(from, to)) => {
+        val (curriesList, res) = extractNCurriedTypesFrom(n - 1, to)
+        (from :: curriesList, res)
+      }
       case (n, other) =>
         throw new ICE("""Function assumed to have more curried arguments than it
                       | actually has""".stripMargin)
