4fb77949153028577dea28c86346e49fa75eb24c
Jackson Woodruff <woodruff.jackson@gmail.com>
2018-01-12 11:57:36 +0000

    Add peephole for boxing and unboxing if statements

--- src/main/scala/target/jvm/JVMPeepholeInstance.scala
+++ src/main/scala/target/jvm/JVMPeepholeInstance.scala
@@ -5,6 +5,21 @@ import peephole.PeepholeInstance
 
 abstract class JVMPeepholeInstance extends PeepholeInstance[JVMInstruction] {
   def compareTo(other: PeepholeInstance[JVMInstruction]): Int = ???
+
+  def isBoxUnboxPair(box: JVMInstruction, unbox: JVMInstruction) =
+    (box, unbox) match {
+      case (JVMInvokeStaticMethod(JVMMethodRef(_: JVMBoxedRef, "valueOf",
+                                               List(_: JVMPrimitiveType),
+                                                _: JVMBoxedType)),
+            JVMInvokeVirtualMethod(JVMMethodRef(_: JVMBoxedRef, name,
+                                                List(),
+                                                _: JVMPrimitiveType))) =>
+        if (name.endsWith("Value"))
+          true
+        else
+          false
+      case _ => false
+  }
 }
 
 /* Turns:
@@ -55,18 +70,46 @@ object BoxUnbox extends JVMPeepholeInstance {
     assert(input.length == 2)
 
     input match {
-      case List(JVMInvokeStaticMethod(JVMMethodRef(_: JVMBoxedRef, "valueOf",
-                                                  List(_: JVMPrimitiveType),
-                                                  _: JVMBoxedType)),
-                JVMInvokeVirtualMethod(JVMMethodRef(_: JVMBoxedRef, name,
-                                                    List(),
-                                                    _: JVMPrimitiveType))) =>
-        if (name.endsWith("Value")) {
+      case List(box, unbox) =>
+        if (isBoxUnboxPair(box, unbox)) {
           Some(List())
         } else {
           None
         }
       case _ => None
+    }
+  }
+}
+
+
+/* This peephole can be deleted when code motion is implemented.  */
+object BranchBoxUnbox extends JVMPeepholeInstance {
+  def getSize = 9
+
+  def matches(input: List[JVMInstruction]) = {
+    assert(input.length == 5)
+
+    input match {
+      case List(cond: JVMConditionalJumpInstruction,
+                loadInstruction1,
+                boxInstruction1,
+                endThenJump: JVMJumpInstruction,
+                elseMark @ JVMLabelMark(elseLabel),
+                loadInstruction2,
+                boxInstruction2,
+                endIfMark @ JVMLabelMark(endIfLabel),
+                unboxInstruction) => {
+        if (cond.getTarget == elseLabel
+            && endThenJump.getTarget == endIfLabel
+            && loadInstruction1.stackEffect == 1
+            && loadInstruction2.stackEffect == 1
+            && isBoxUnboxPair(boxInstruction1, unboxInstruction)
+            && isBoxUnboxPair(boxInstruction2, unboxInstruction))
+          Some(List(cond, loadInstruction1, endThenJump,
+                    elseMark, loadInstruction2, endIfMark))
+        else
+          None
+      }
     }
   }
 }
