1dec0a5d17be0c75127a957f582001acd2c032af
Jackson Woodruff <woodruff.jackson@gmail.com>
2018-02-13 16:34:47 +0000

    Add equality checks to the Datatypes

--- stdlib/cmlc/Datatype.java
+++ stdlib/cmlc/Datatype.java
@@ -13,4 +13,31 @@ public abstract class Datatype extends Function {
 	public Object unapply() {
 		return value;
 	}
+
+	public boolean equals(Object other) {
+		if (!(other instanceof Datatype)) {
+			return false;
+		} else {
+			Datatype otherDatatype = (Datatype) other;
+
+			if (otherDatatype.id() != id()) {
+				return false;
+			}
+
+			Object thisUnapply = this.unapply();
+			Object otherUnapply = otherDatatype.unapply();
+
+			if (thisUnapply == null && otherUnapply == null) {
+				return true;
+			} else if (thisUnapply == null || otherUnapply == null) {
+				return false;
+			} else {
+				return thisUnapply.equals(otherUnapply);
+			}
+		}
+	}
+
+	/* This is to return a different ID for each datatype instance
+	 * implemented.  */
+	public abstract int id();
 }
